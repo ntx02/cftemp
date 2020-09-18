8e258058531622d1b70337f6c924b2f34723d6e4
Elijah N <57156663+ntx02@users.noreply.github.com>
2020-09-18 00:23:46 -0400

    Update temperature program.py

--- temperature program.py
+++ temperature program.py
@@ -1,3 +1,40 @@
-import base64
-exec(base64.b64decode(b'ZnJvbSBjdHlwZXMgaW1wb3J0IHdpbmRsbCwgY19jaGFyX3AKaW1wb3J0IHN1YnByb2Nlc3MgYXMgc3AKaW1wb3J0IHRpbWUKCkVNX1JFUExBQ0VTRUwgPSAxOTQKCmRlZiBmaW5kKHRpdGxlKToKICAgIHJldHVybiB3aW5kbGwudXNlcjMyLkZpbmRXaW5kb3dBKE5vbmUsIGNfY2hhcl9wKHRpdGxlLmVuY29kZSgpKSkKCmRlZiBzcGF3bl9sb2coKToKICAgIHNwLlBvcGVuKFsnbm90ZXBhZC5leGUnXSkKICAgIHRpbWUuc2xlZXAoMC4wOCkKICAgIHJldHVybiBmaW5kKCJVbnRpdGxlZCAtIE5vdGVwYWQiKQoKZGVmIGxvZyhsb2dfc3RyLCBtYWtlX25ldz1GYWxzZSwgZW5kPSdcbicpOgogICAgaHdkID0gZmluZCgiVW50aXRsZWQgLSBOb3RlcGFkIikgb3IgZmluZCgiKlVudGl0bGVkIC0gTm90ZXBhZCIpIG9yIChtYWtlX25ldyBhbmQgc3Bhd25fbG9nKCkpCiAgICBpZiBub3QgaHdkOgogICAgICAgIHJldHVybgogICAgZWRpdCA9IHdpbmRsbC51c2VyMzIuRmluZFdpbmRvd0V4QShod2QsIE5vbmUsIGNfY2hhcl9wKGIiRURJVCIpLCBOb25lKQogICAgbG9nX3N0ciA9IGxvZ19zdHIucmVwbGFjZSgnXDAnLCcnKSArIGVuZCArICdcMCcKICAgIHdpbmRsbC51c2VyMzIuU2VuZE1lc3NhZ2VBKGVkaXQsIEVNX1JFUExBQ0VTRUwsIEZhbHNlLCBjX2NoYXJfcChsb2dfc3RyLmVuY29kZSgpKSkKCmxvZygnWW91XCd2ZSBiZWVuIHJvb3RlZCcsIFRydWUp').decode())
+print("This is a program written by Elijah to convert between Fahrenheit and Celsius")
+print(" ")
+
+def celsiusfunc():
+    print("What is value in Celsius?")
+    print(" ")
+    initialc = float(input(">"))
+    def c_to_f(initialc):
+        f_temp = float(initialc) * (9/5) + 32
+        return f_temp
+    print(" ")
+    print("Value in Fahrenheit is " + str(c_to_f(initialc)))
+    print(" ")
     
+def fahrenheitfunc():
+    print("What is value in Fahrenheit?")
+    print(" ")
+    initialf = float(input(">"))
+    def f_to_c(initialf):
+        c_temp = (float(initialf)-32) * (5/9)
+        return c_temp
+    print(" ")
+    print("Value in Celsius is " + str(f_to_c(initialf)))
+    print(" ")
+
+x = 1
+while True:
+    print("Is temperature in Fahrenheit or Celsius? (answer with 'f' or 'c'). If you wish to end this program, type 'end'.")
+    print(" ")
+    f_or_c = input("\n> ").lower()
+    print(" ")
+    if f_or_c == "c":
+        celsiusfunc()
+    if f_or_c == "f":
+        fahrenheitfunc()
+    if f_or_c == "end":
+        break
+    if f_or_c != "f" or "c":
+        print("\nPlease input a value of 'f' or 'c'.")
+        print(" ")
